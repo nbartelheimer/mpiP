038f29e518b1b23250dd825f4ba4d6ccf1d28b3a
Niklas Bartelheimer <u0001670@login1.cloud.r-ccs.riken.jp>
2026-03-10 22:36:34 +0900

    added some error checking and output

--- mpiPi_papi.c
+++ mpiPi_papi.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "mpiPi_papi.h"
 
 int llc_event_set = -1;
@@ -6,12 +7,31 @@ int llc_event_set = -1;
 void papi_module_init(){
 	char *counter_name = NULL;
 	int event_code = PAPI_NULL;
-	PAPI_library_init(PAPI_VER_CURRENT);
+	int ret = -1;
+	if((ret = PAPI_library_init(PAPI_VER_CURRENT)) != PAPI_VER_CURRENT){
+		fprintf(stderr,"[PAPI_MODULE_ERROR] Version mismatch! Requried: %i Actual: %i\n",PAPI_VER_CURRENT,ret);
+		exit(EXIT_FAILURE);
+	}
 	llc_event_set = PAPI_NULL;
-	PAPI_create_eventset(&llc_event_set);
+	if((ret = PAPI_create_eventset(&llc_event_set)) != PAPI_OK){
+		fprintf(stderr,"[PAPI_MODULE_ERROR] %s in %s at %d\n", PAPI_strerror(ret),__FILE__,__LINE__);
+		exit(EXIT_FAILURE);
+	}
 	counter_name = getenv("MPIP_PAPI_EVENT_NAME");
-	PAPI_event_name_to_code(counter_name, &event_code);
-	PAPI_add_event(llc_event_set,event_code);
+	if(counter_name == NULL){
+		fprintf(stderr,"[PAPI_MODULE_ERROR] MPIP_PAPI_EVENT_NAME cannot be empty!\n");
+		exit(EXIT_FAILURE);
+	}
+	if((ret = PAPI_event_name_to_code(counter_name, &event_code)) != PAPI_OK){
+		fprintf(stderr,"[PAPI_MODULE_ERROR] %s in %s at %d\n", PAPI_strerror(ret),__FILE__,__LINE__);
+		exit(EXIT_FAILURE);
+	}
+
+	if((ret = PAPI_add_event(llc_event_set,event_code)) != PAPI_OK){
+		fprintf(stderr,"[PAPI_MODULE_ERROR] %s in %s at %d\n", PAPI_strerror(ret),__FILE__,__LINE__);
+		exit(EXIT_FAILURE);
+
+	}
 }
 
 void papi_module_finalize(){
